6282e85366c9412cd3e332466a6e8d90fc27d56d
yyyoichi <origamiichiro@gmail.com>
2024-03-05 11:40:19 +0000

    ref no-use connect

--- .devcontainer/init-devcontainer.sh
+++
@@ -1,4 +0,0 @@
-go install github.com/bufbuild/buf/cmd/buf@latest && \
-go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest && \
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest && \
-go install connectrpc.com/connect/cmd/protoc-gen-connect-go@latest
--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/yyyoichi/OhAuth0.1
 
 go 1.22.0
-
-require (
-	connectrpc.com/connect v1.15.0
-	google.golang.org/protobuf v1.32.0
-)
