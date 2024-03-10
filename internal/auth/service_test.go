aa772521ffab82281b47c291881326b3b9f2e646
yyyoichi <origamiichiro@gmail.com>
2024-03-10 12:17:02 +0000

    feat remote db test with rpc

--- internal/auth/service_test.go
+++ internal/auth/service_test.go
@@ -13,126 +13,89 @@ import (
 )
 
 func TestService(t *testing.T) {
-	test := map[string]struct {
-		tservice *Service
-	}{
-		"local": {func() *Service {
-			db, _ := database.NewDatabase()
-			return &Service{
-				client: db,
-			}
-		}()},
-		"remote": {func() *Service {
-			port := "3336"
-			err := database.NewDatabaseServer(database.ServerConfig{
-				Port: port,
-			})
-			assert.NoError(t, err)
-			ctx := context.Background()
-			var service *Service
-			connectedCh := make(chan interface{})
-			go func() {
-				defer close(connectedCh)
-				for {
-					time.Sleep(time.Duration(10) * time.Millisecond)
-					var err error
-					service, err = NewService(ctx, Config{
-						DatabaseServerURL: "http://localhost:" + port,
-					})
-					if err != nil {
-						continue
-					}
-					if _, err := service.client.GetUserByID(context.Background(), "1"); err != nil {
-						continue
-					}
-					return
-				}
-			}()
-			<-connectedCh
-			return service
-		}()},
+	newLocalService := func() *Service {
+		db, _ := database.NewDatabase()
+		return &Service{
+			client: db,
+		}
 	}
-	for scenario, tt := range test {
-		t.Run(scenario, func(t *testing.T) {
-			t.Run("JWT", func(t *testing.T) {
-				t.Parallel()
-				ctx := context.Background()
-				tservice := tt.tservice
-				claims, err := tservice.Authentication(ctx, "1", "password")
-				assert.NoError(t, err)
-				claims.ClientId = "hoge"
-				assert.NoError(t, err)
-				token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-				ss, err := token.SignedString([]byte("secret"))
-				assert.NoError(t, err)
+	t.Run("JWT", func(t *testing.T) {
+		t.Parallel()
+		ctx := context.Background()
+		tservice := newLocalService()
+		claims, err := tservice.Authentication(ctx, "1", "password")
+		assert.NoError(t, err)
+		claims.ClientId = "hoge"
+		assert.NoError(t, err)
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		ss, err := token.SignedString([]byte("secret"))
+		assert.NoError(t, err)
 
-				// parse
-				act, err := tservice.ParseMyClaims(ctx, ss, []byte("secret"))
-				assert.NoError(t, err)
-				sub, err := act.GetSubject()
-				assert.NoError(t, err)
-				assert.Equal(t, "1", sub)
-				assert.Equal(t, "hoge", act.ClientId)
-			})
-			t.Run("code to accesstoken refresh", func(t *testing.T) {
-				t.Parallel()
-				ctx := context.Background()
-				tservice := tt.tservice
-				const (
-					USER_ID   = "TESTING_USER"
-					CLIENT_ID = "TESTING_CLIENT"
-				)
-				code, err := tservice.NewAuthorizationCode(ctx, NewAuthorizationCodeConfig{
-					UserId:          USER_ID,
-					ServiceClientId: CLIENT_ID,
-				})
-				assert.NoError(t, err)
-				assert.Equal(t, USER_ID, code.UserId)
-				assert.Equal(t, CLIENT_ID, code.ServiceClientId)
-				assert.NotEmpty(t, code.Code)
-				assert.False(t, code.Expires.AsTime().IsZero())
-
-				testTokens := func(token *apiv1.AccessToken, refresh *apiv1.RefreshToken) {
-					assert.NoError(t, err)
-					assert.Equal(t, USER_ID, token.UserId)
-					assert.Equal(t, CLIENT_ID, token.ServiceClientId)
-					assert.NotEmpty(t, token.Token)
-					assert.False(t, code.Expires.AsTime().IsZero())
-
-					assert.Equal(t, USER_ID, refresh.UserId)
-					assert.Equal(t, CLIENT_ID, refresh.ServiceClientId)
-					assert.NotEmpty(t, refresh.Token)
-					assert.False(t, refresh.Expires.AsTime().IsZero())
-				}
-				token, refresh, err := tservice.NewAccessToken(ctx, code.Code)
-				testTokens(token, refresh)
-				token, refresh, err = tservice.UpdateAccessToken(ctx, refresh.Token)
-				testTokens(token, refresh)
-			})
-
-			t.Run("expired", func(t *testing.T) {
-				t.Parallel()
-				ctx := context.Background()
-				tservice := tt.tservice
-
-				err := tservice.client.CreateAuthorizationCode(ctx, &apiv1.AuthorizationCode{
-					Code:    "example",
-					Expires: timestamppb.New(time.Now().Add(time.Duration(-1) * time.Minute)),
-				})
-				assert.NoError(t, err)
-
-				_, _, err = tservice.NewAccessToken(ctx, "example")
-				assert.ErrorIs(t, ErrAuthorizationCodeExpired, err)
-
-				err = tservice.client.CreateRefreshToken(ctx, &apiv1.RefreshToken{
-					Token:   "example",
-					Expires: timestamppb.New(time.Now().Add(time.Duration(-1) * time.Minute)),
-				})
-				assert.NoError(t, err)
-				_, _, err = tservice.UpdateAccessToken(ctx, "example")
-				assert.ErrorIs(t, ErrRefreshTokenExpired, err)
-
-			})
+		// parse
+		act, err := tservice.ParseMyClaims(ctx, ss, []byte("secret"))
+		assert.NoError(t, err)
+		sub, err := act.GetSubject()
+		assert.NoError(t, err)
+		assert.Equal(t, "1", sub)
+		assert.Equal(t, "hoge", act.ClientId)
+	})
+	t.Run("code to accesstoken refresh", func(t *testing.T) {
+		t.Parallel()
+		ctx := context.Background()
+		tservice := newLocalService()
+		const (
+			USER_ID   = "TESTING_USER"
+			CLIENT_ID = "TESTING_CLIENT"
+		)
+		code, err := tservice.NewAuthorizationCode(ctx, NewAuthorizationCodeConfig{
+			UserId:          USER_ID,
+			ServiceClientId: CLIENT_ID,
 		})
-	}
+		assert.NoError(t, err)
+		assert.Equal(t, USER_ID, code.UserId)
+		assert.Equal(t, CLIENT_ID, code.ServiceClientId)
+		assert.NotEmpty(t, code.Code)
+		assert.False(t, code.Expires.AsTime().IsZero())
+
+		testTokens := func(token *apiv1.AccessToken, refresh *apiv1.RefreshToken) {
+			assert.NoError(t, err)
+			assert.Equal(t, USER_ID, token.UserId)
+			assert.Equal(t, CLIENT_ID, token.ServiceClientId)
+			assert.NotEmpty(t, token.Token)
+			assert.False(t, code.Expires.AsTime().IsZero())
+
+			assert.Equal(t, USER_ID, refresh.UserId)
+			assert.Equal(t, CLIENT_ID, refresh.ServiceClientId)
+			assert.NotEmpty(t, refresh.Token)
+			assert.False(t, refresh.Expires.AsTime().IsZero())
+		}
+		token, refresh, err := tservice.NewAccessToken(ctx, code.Code)
+		testTokens(token, refresh)
+		token, refresh, err = tservice.UpdateAccessToken(ctx, refresh.Token)
+		testTokens(token, refresh)
+	})
+
+	t.Run("expired", func(t *testing.T) {
+		t.Parallel()
+		ctx := context.Background()
+		tservice := newLocalService()
+
+		err := tservice.client.CreateAuthorizationCode(ctx, &apiv1.AuthorizationCode{
+			Code:    "example",
+			Expires: timestamppb.New(time.Now().Add(time.Duration(-1) * time.Minute)),
+		})
+		assert.NoError(t, err)
+
+		_, _, err = tservice.NewAccessToken(ctx, "example")
+		assert.ErrorIs(t, ErrAuthorizationCodeExpired, err)
+
+		err = tservice.client.CreateRefreshToken(ctx, &apiv1.RefreshToken{
+			Token:   "example",
+			Expires: timestamppb.New(time.Now().Add(time.Duration(-1) * time.Minute)),
+		})
+		assert.NoError(t, err)
+		_, _, err = tservice.UpdateAccessToken(ctx, "example")
+		assert.ErrorIs(t, ErrRefreshTokenExpired, err)
+
+	})
 }
--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -2,14 +2,109 @@ package database
 
 import (
 	"context"
-	"log"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	apiv1 "github.com/yyyoichi/OhAuth0.1/api/v1"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func TestDb(t *testing.T) {
+func TestDatabase(t *testing.T) {
+	test := map[string]databaseInterface{
+		"local": func() *Database {
+			db, _ := NewDatabase()
+			return db
+		}(),
+		"remote": func() *Client {
+			port := "3366"
+			err := NewDatabaseServer(ServerConfig{
+				Port: port,
+			})
+			assert.NoError(t, err)
+			var client *Client
+			connectedCh := make(chan struct{})
+			go func() {
+				defer close(connectedCh)
+				for {
+					ctx := context.Background()
+					var err error
+					client, err = NewDatabaseClient(ctx, ClientConfig{
+						URL: "http://localhost:" + port,
+					})
+					if err != nil {
+						continue
+					}
+					if err := client.Ping(ctx); err != nil {
+						continue
+					}
+					return
+				}
+			}()
+			<-connectedCh
+			return client
+		}(),
+	}
+	for scenario, tt := range test {
+		t.Run(scenario, func(t *testing.T) {
+			testdb(t, tt)
+		})
+	}
+}
+
+func testdb(t *testing.T, db databaseInterface) {
 	ctx := context.Background()
-	db, _ := NewDatabase()
-	c, _ := db.GetServieClientByID(ctx, "500")
-	c.Name = "rename"
-	log.Println(db.serviceClientByID["500"].Name)
+	user, err := db.GetUserByID(ctx, "1")
+	assert.NoError(t, err)
+	assert.NotZero(t, user)
+	client, err := db.GetServieClientByID(ctx, "500")
+	assert.NoError(t, err)
+	assert.NotZero(t, client)
+	NOW := timestamppb.Now()
+	expcode := &apiv1.AuthorizationCode{
+		Code:            "code",
+		UserId:          "11",
+		ServiceClientId: "222",
+		Expires:         NOW,
+		Scope:           "hoge",
+	}
+	err = db.CreateAuthorizationCode(ctx, expcode)
+	assert.NoError(t, err)
+	code, err := db.GetAuthorizationCodeByCode(ctx, expcode.Code)
+	assert.NoError(t, err)
+	assert.EqualExportedValues(t, expcode, code)
+	exptoken := &apiv1.AccessToken{
+		Token:           "token",
+		UserId:          "11",
+		ServiceClientId: "222",
+		Expires:         NOW,
+		Scope:           "hoge",
+	}
+	err = db.CreateAccessToken(ctx, exptoken)
+	assert.NoError(t, err)
+	token, err := db.GetAccessTokenByToken(ctx, exptoken.Token)
+	assert.NoError(t, err)
+	assert.EqualExportedValues(t, exptoken, token)
+	exprefresh := &apiv1.RefreshToken{
+		Token:           "token",
+		UserId:          "11",
+		ServiceClientId: "222",
+		Expires:         NOW,
+		Scope:           "hoge",
+	}
+	err = db.CreateRefreshToken(ctx, exprefresh)
+	assert.NoError(t, err)
+	refresh, err := db.GetRefreshTokenByToken(ctx, exprefresh.Token)
+	assert.NoError(t, err)
+	assert.EqualExportedValues(t, exprefresh, refresh)
+}
+
+type databaseInterface interface {
+	GetUserByID(ctx context.Context, id string) (*apiv1.UserProfile, error)
+	GetServieClientByID(ctx context.Context, id string) (*apiv1.ServiceClient, error)
+	GetAuthorizationCodeByCode(ctx context.Context, code string) (*apiv1.AuthorizationCode, error)
+	CreateAuthorizationCode(ctx context.Context, row *apiv1.AuthorizationCode) error
+	GetAccessTokenByToken(ctx context.Context, token string) (*apiv1.AccessToken, error)
+	CreateAccessToken(ctx context.Context, row *apiv1.AccessToken) error
+	GetRefreshTokenByToken(ctx context.Context, token string) (*apiv1.RefreshToken, error)
+	CreateRefreshToken(ctx context.Context, row *apiv1.RefreshToken) error
 }
