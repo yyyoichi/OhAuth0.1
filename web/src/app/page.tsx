c60828bcc3b30f33068e8ffc588807dabccb21ae
yyyoichi <origamiichiro@gmail.com>
2024-03-14 14:39:22 +0000

    feat next.js project init

---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-bet ween p-24">
+			<div>
+				<h1>Hello world</h1>
+			</div>
+		</main>
+	);
+}
