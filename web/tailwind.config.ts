f2a79b9acf3dbbde564e7a3c8c92d277b75f9c56
yyyoichi <origamiichiro@gmail.com>
2024-03-16 05:27:16 +0000

    feat add color pallet

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -13,6 +13,56 @@ const config: Config = {
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+			colors: {
+				"my-black": "var(--my-black)",
+				"my-light-black": "var(--my-light-black)",
+				"my-white": "var(--my-white)",
+				"my-red": "var(--my-red)",
+				"my-green": "var(--my-green)",
+				"my-yellow": "var(--my-yellow)",
+				"myc-black": "var(--myc-black)",
+				"myc-light-black": "var(--myc-light-black)",
+				"myc-white": "var(--myc-white)",
+				"myc-red": "var(--myc-red)",
+				"myc-green": "var(--myc-green)",
+				"myc-yellow": "var(--myc-yellow)",
+				"myc-fff": "var(--myc-fff)",
+			},
+			keyframes: {
+				wiggle: {
+					"0%, 100%": { transform: "rotate(-3deg)" },
+					"50%": { transform: "rotate(3deg)" },
+				},
+				fadein: {
+					"0%": {
+						opacity: "0",
+						transform: "translateY(5px)",
+						height: "0",
+					},
+					"100%": {
+						opacity: "1",
+						transform: "translateY(0)",
+						height: "inherit",
+					},
+				},
+				fadeout: {
+					"0%": {
+						opacity: "1",
+						transform: "translateY(-2px)",
+						height: "inherit",
+					},
+					"100%": {
+						opacity: "0",
+						transform: "translateY(0)",
+						height: "0",
+					},
+				},
+			},
+			animation: {
+				wiggle: "wiggle 1s ease-in-out infinite",
+				fadein: "fadein .8s forwards",
+				fadeout: "fadeout .5s forwards",
+			},
 		},
 	},
 	plugins: [],
